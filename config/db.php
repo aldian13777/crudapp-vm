4b67569f3f3ed32d33108aefe213a719ac6e77a3
aldian13777 <aldian13777@gmail.com>
2026-01-08 09:19:44 +0800

    pdo learning

--- config/db.php
+++ config/db.php
@@ -1,22 +1,24 @@
 <?php
-// db.php
-
+// Data source name parameters (host,port,db)
 $host = 'ep-restless-dust-aekvtftd.c-2.us-east-2.aws.neon.tech';
 $port = 5432;
 $db   = 'crudapp';
+
+//Data Source Name (DSN) variable
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+
+// Database user & pass
 $user = 'neondb_owner';
 $pass = 'npg_z9PjKMr5dUWa';
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+// PDO Connection variable
+$pdo = new PDO($dsn,$user,$pass);
 
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+// test connection
+try{
+    $pdo;
+    echo "Connected";
+}
+catch (PDOException $e){
+    echo "Error";
 }
--- config/fetch.php
+++
@@ -1,9 +0,0 @@
-<?php
-// fetch.php
-
-require 'db.php';
-
-$sql = 'SELECT "Name","Age" FROM "Identity"';
-
-$stmt = $pdo->query($sql);
-$datas = $stmt->fetchAll();
